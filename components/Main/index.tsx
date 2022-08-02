d3bc1e49e6d3c44d6d3826b170fc12f4c01ab129
YoungJu <huey2269@gmail.com>
2022-08-02 15:40:57 +0900

    Banner_1

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,18 +1,56 @@
 import React from "react";
 import * as S from "./styles";
+import Image from "next/image";
+
+// h1 32px;  h2 20px; h3 18px;
 
 const MainComponent = () => {
   return (
-    <>
+    <S.styledDiv>
+      <S.circle1 />
+      <S.Rcircle1 />
+      <S.Rcircle2 />
+      <S.Rcircle3 />
       <S.BgContainer1>
-        <S.circle1 />
-        <S.Rcircle1 />
-        <S.Rcircle2 />
-        <S.Rcircle3 />
+        <S.textDiv>
+          <S.titleDiv>
+            <h2>교육형 버그바운티 플랫폼</h2>
+            <h1>HackingField!</h1>
+          </S.titleDiv>
+          <S.Bar />
+          <S.bodyDiv>
+            <h3>
+              버그바운티 플랫폼 "HackingField"는 주니어 개발자와 주니어 해커들이
+              새로운 경험을 쌓고 서로의 성장을
+              <br />
+              이끌어내는 환경을 제공합니다.
+            </h3>
+          </S.bodyDiv>
+          <S.iconBox>
+            <S.jpBox>
+              <Image
+                src="/images/Icon_junior programmer.svg"
+                alt="jp"
+                width={77}
+                height={80}
+              />
+              <h3>주니어 개발자</h3>
+            </S.jpBox>
+            <S.jhBox>
+              <Image
+                src="/images/Icon_junior hacker.svg"
+                alt="jh"
+                width={77}
+                height={80}
+              />
+              <h3>주니어 해커</h3>
+            </S.jhBox>
+          </S.iconBox>
+        </S.textDiv>
       </S.BgContainer1>
-      <S.BgContainer2 />
-      <S.BgContainer3 />
-    </>
+      <S.BgContainer2></S.BgContainer2>
+      <S.BgContainer3></S.BgContainer3>
+    </S.styledDiv>
   );
 };
 
--- components/Main/styles.tsx
+++ components/Main/styles.tsx
@@ -1,9 +1,102 @@
 import styled from "styled-components";
 
+export const styledDiv = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+  color: #ffffff;
+  font-weight: normal;
+
+  h1 {
+    font-size: 32px;
+    margin: 0px;
+  }
+  h2 {
+    font-size: 20px;
+    margin: 0px;
+  }
+  h3 {
+    font-size: 18px;
+    margin: 0px;
+  }
+`;
+
 export const Wrapper = styled.div`
   position: absolute;
   width: 100vw;
   height: 3000px;
+`;
+
+export const textDiv = styled.div`
+  position: relative;
+  top: 300px;
+  left: 480px;
+  width: 500px;
+  height: 600px;
+  // background-color: black;
+`;
+
+export const titleDiv = styled.div`
+  width: 230px;
+  height: 80px;
+  position: relative;
+  top: 0px;
+  margin: 0 auto;
+  text-align: center;
+
+  h1 {
+    font-weight: 700;
+  }
+
+  h2 {
+    font-weight: 400;
+  }
+`;
+
+export const Bar = styled.div`
+  position: absolute;
+  top: 160px;
+  left: 43%;
+  width: 72px;
+  height: 1px;
+  border: 1px solid white;
+  background-color: white;
+`;
+
+export const bodyDiv = styled.div`
+  width: 420px;
+  height: 100px;
+  top: 112px;
+  text-align: center;
+  margin: 0 auto;
+  position: relative;
+`;
+
+export const iconBox = styled.div`
+  width: 420px;
+  height: 130px;
+  top: 200px;
+  display: flex;
+  text-align: center;
+  margin: 0 auto;
+  position: relative;
+  //background-color: rosybrown;
+  h3 {
+    font-size: 16px;
+    font-weight: 200;
+  }
+`;
+
+export const jpBox = styled.div`
+  width: 100px;
+  height: 80px;
+  left: 50px;
+  position: relative;
+`;
+
+export const jhBox = styled.div`
+  width: 100px;
+  height: 80px;
+  left: 180px;
+  position: relative;
 `;
 
 export const BgContainer1 = styled.div`
@@ -42,7 +135,7 @@ export const BgContainer3 = styled.div`
 
 export const circle1 = styled.div`
   position: fixed;
-  z-index: 10;
+  z-index: 1;
   top: -376px;
   left: 128px;
   width: 624px;
@@ -53,10 +146,10 @@ export const circle1 = styled.div`
 
 export const Rcircle1 = styled.div`
   position: fixed;
-  z-index: 10;
+  z-index: 1;
   width: 1200px;
   height: 600px;
-  right: -11%;
+  right: -20%;
   border: 2px dashed #1cb56d;
   bottom: 280px;
   background-size: 40px 10px;
@@ -67,10 +160,10 @@ export const Rcircle1 = styled.div`
 
 export const Rcircle2 = styled.div`
   position: fixed;
-  z-index: 10;
+  z-index: 1;
   width: 1200px;
   height: 600px;
-  right: -12%;
+  right: -20%;
   border: 1px solid #1cb56d;
   bottom: 100px;
   background-size: 40px 10px;
@@ -81,10 +174,10 @@ export const Rcircle2 = styled.div`
 
 export const Rcircle3 = styled.div`
   position: fixed;
-  z-index: 10;
+  z-index: 1;
   width: 1200px;
   height: 600px;
-  right: -11%;
+  right: -20%;
   border: 2px dashed #1cb56d;
   bottom: -100px;
   background-size: 40px 10px;
