c5606791a51728e701b477b2be64b770e11b02c7
jinhyuk <wlsgur0324@naver.com>
2022-08-23 14:13:39 +0900

    Header 페이지 변경

--- components/Common/Layout/styles.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const LayoutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import Layout from "../components/Common/Layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return pageProps.pathname === "/" ||
-    pageProps.pathname === "/login" ||
-    pageProps.pathname === "/signup/common" ? (
+  return (
     <RecoilRoot>
       <Component {...pageProps} />
-    </RecoilRoot>
-  ) : (
-    <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
     </RecoilRoot>
   );
 };
 
-App.getInitialProps = async (context: { ctx: any; Component: any }) => {
-  const { ctx, Component } = context;
-  let pageProps = {};
-
-  if (Component.getInitialProps) {
-    // Component (pages 폴더에 있는 컴포넌트)에 getInitialProps가 있다면
-    pageProps = (await Component.getInitialProps(ctx)) || {};
-
-    return { pageProps };
-  }
-};
 export default App;
