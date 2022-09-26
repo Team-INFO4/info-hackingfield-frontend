d4469c4785a5a5c2d5102c14b8a06ae36e460ea9
jinhyuk <wlsgur0324@naver.com>
2022-09-27 08:48:24 +0900

    레포지터리 수정 전 작업 분량

---
+++ components/Raking/styles.tsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+
+export const HomeWrapper = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  background-color: #0d1117;
+  overflow: hidden;
+`;
+
+export const Title = styled.div`
+  height: 26px;
+  margin: 100px auto 0px auto;
+  font-weight: 500;
+  font-size: 18px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export const Selected = styled.div`
+  width: 34px;
+  height: 0;
+  margin: 13px auto;
+  border: 1px solid #ffffff;
+  text-align: center;
+`;
+
+export const TopRank = styled.div`
+  position: relative;
+  width: 1180px;
+  margin: 92px auto;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Top = styled.div`
+  width: 180px;
+  margin-top: -20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const second = styled.div`
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+`;
